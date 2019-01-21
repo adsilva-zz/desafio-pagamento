0ae91a5449cc54fa4f8a0edf2e18d05c1b11ff31
Amanda da Silva Ito <adsilva32@gmail.com>
2019-01-21 14:57:33 -0200

    Configurando Projeto Pagamento

---
+++ src/test/java/com/desafio/pagamento/PagamentoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.desafio.pagamento;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PagamentoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
