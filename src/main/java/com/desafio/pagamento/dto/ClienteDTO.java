363654b4d2ac069c1affbdec78ec7d5d8dff4b5c
Amanda da Silva Ito <adsilva32@gmail.com>
2019-01-23 17:57:11 -0200

    Atualização ClienteDTO

--- src/main/java/com/desafio/pagamento/dto/ClienteDTO.java
+++ src/main/java/com/desafio/pagamento/dto/ClienteDTO.java
@@ -1,41 +1,24 @@
 package com.desafio.pagamento.dto;
 
-import javax.validation.constraints.NotEmpty;
-
-import org.hibernate.validator.constraints.br.CNPJ;
+import javax.validation.constraints.NotNull;
 
 public class ClienteDTO {
 
-	@NotEmpty
-	private String razaoSocial;
-	@CNPJ
-	private String cnpj;
+	@NotNull
+	private Long idCliente;
 
-	public ClienteDTO(String razaoSocial, String cnpj) {
+	public Long getIdCliente() {
+		return idCliente;
+	}
+
+	public ClienteDTO(Long idCliente) {
 		super();
-		this.razaoSocial = razaoSocial;
-		this.cnpj = cnpj;
-	}
-
-	public String getRazaoSocial() {
-		return razaoSocial;
-	}
-
-	public void setRazaoSocial(String razaoSocial) {
-		this.razaoSocial = razaoSocial;
-	}
-
-	public String getCnpj() {
-		return cnpj;
-	}
-
-	public void setCnpj(String cnpj) {
-		this.cnpj = cnpj;
+		this.idCliente = idCliente;
 	}
 
 	@Override
 	public String toString() {
-		return "ClienteDTO [razaoSocial=" + razaoSocial + ", CNPJ=" + cnpj + "]";
+		return "ClienteDTO [idCliente=" + idCliente + "]";
 	}
 
 }
