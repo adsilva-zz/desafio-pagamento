ee5a00da1b0821eb7550312d119e518061aaf818
Amanda da Silva Ito <adsilva32@gmail.com>
2019-01-23 17:59:35 -0200

    Criando camada Servico e ClienteServico

---
+++ src/main/java/com/desafio/pagamento/repositorio/ClienteRepositorio.java
@@ -0,0 +1,11 @@
+package com.desafio.pagamento.repositorio;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.desafio.pagamento.entidade.Cliente;
+
+@Repository
+public interface ClienteRepositorio extends JpaRepository<Cliente, Long> {
+
+}
