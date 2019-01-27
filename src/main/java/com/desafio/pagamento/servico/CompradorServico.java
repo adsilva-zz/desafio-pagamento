9b129185ab1eb25c6dabb50290ec90cf4e78b814
Amanda da Silva Ito <adsilva32@gmail.com>
2019-01-27 10:50:43 -0200

    Adicionado CompradorServico na camada Servico

---
+++ src/main/java/com/desafio/pagamento/servico/CompradorServico.java
@@ -0,0 +1,8 @@
+package com.desafio.pagamento.servico;
+
+import com.desafio.pagamento.entidade.Comprador;
+
+public interface CompradorServico {
+
+	public Comprador buscarComprador(Comprador comprador);
+}
---
+++ src/main/java/com/desafio/pagamento/servico/impl/CompradorServicoImpl.java
@@ -0,0 +1,21 @@
+package com.desafio.pagamento.servico.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.desafio.pagamento.entidade.Comprador;
+import com.desafio.pagamento.repositorio.CompradorRepositorio;
+import com.desafio.pagamento.servico.CompradorServico;
+
+@Service
+public class CompradorServicoImpl implements CompradorServico {
+
+	@Autowired
+	private CompradorRepositorio compradorRepositorio;
+
+	@Override
+	public Comprador buscarComprador(Comprador comprador) {
+		return compradorRepositorio.findById(comprador.getIdComprador()).get();
+	}
+
+}
