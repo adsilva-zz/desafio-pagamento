f2a447dc52bc5506a8af65f2501168bfd1553c0f
Amanda da Silva Ito <adsilva32@gmail.com>
2019-01-28 21:55:43 -0200

    Ajuste do converte de pagamento

--- src/main/java/com/desafio/pagamento/converte/DtoToPagamentoConverte.java
+++ src/main/java/com/desafio/pagamento/converte/DtoToPagamentoConverte.java
@@ -3,10 +3,12 @@ package com.desafio.pagamento.converte;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
 
 import com.desafio.pagamento.dto.PagamentoDTO;
 import com.desafio.pagamento.entidade.FormaPagamento;
 import com.desafio.pagamento.entidade.Pagamento;
+import com.desafio.pagamento.exception.CartaoNuloException;
 
 @Component
 public class DtoToPagamentoConverte implements Converter<PagamentoDTO, Pagamento> {
@@ -18,6 +20,9 @@ public class DtoToPagamentoConverte implements Converter<PagamentoDTO, Pagamento
 	public Pagamento convert(PagamentoDTO pagamentoDTO) {
 		Pagamento pagamento = new Pagamento();
 		if (FormaPagamento.CARTAO_CREDITO.equals(pagamentoDTO.getForma())) {
+			if (ObjectUtils.isEmpty(pagamentoDTO.getCartao())) {
+				throw new CartaoNuloException();
+			}
 			pagamento.setCartao(cartaoConverte.convert(pagamentoDTO.getCartao()));
 		}
 
