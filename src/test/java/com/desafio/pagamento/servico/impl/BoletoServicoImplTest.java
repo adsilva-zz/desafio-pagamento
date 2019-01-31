354b92c1e8e66564972e75ecbd7a9eb5fd5da493
Amanda da Silva Ito <adsilva32@gmail.com>
2019-01-31 14:37:46 -0200

    Inclusão da classe de teste BoletoServicoImplTest

---
+++ src/test/java/com/desafio/pagamento/servico/impl/BoletoServicoImplTest.java
@@ -0,0 +1,35 @@
+package com.desafio.pagamento.servico.impl;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.desafio.pagamento.entidade.Boleto;
+import com.desafio.pagamento.servico.BoletoServico;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+public class BoletoServicoImplTest {
+
+	@Autowired
+	private BoletoServico boletoServico;
+
+	Boleto boleto = new Boleto();
+
+	@Test
+	public void gerarBoletoComSucesso() {
+		Boleto gerarBoleto = boletoServico.gerarBoleto();
+		Assert.assertNotNull(gerarBoleto);
+	}
+
+	@Test
+	public void salvarBoletoComSucesso() {
+		Boleto boleto = boletoServico.gerarBoleto();
+		Assert.assertEquals(boleto, boletoServico.salvarBoleto(boleto));
+	}
+}
